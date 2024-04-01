505e5f8989b0aef894cf79c41e9d6900204a0aee
duxiuxing <duxiuxing@foxmail.com>
2024-04-02 04:47:04 +0800

    add game-list on cn

---
+++ game-list/script/local_settings.py
@@ -0,0 +1,5 @@
+#-- coding: UTF-8 --
+
+class LocalSettings:
+    wiitdb_en_txt = "C:\\workspace\\github\\duxiuxing\\wii-player\\game-list\\wiitdb.en.txt"
+    wiitdb_zhcn_txt = "C:\\workspace\\github\\duxiuxing\\wii-player\\game-list\\wiitdb.zhcn.txt"
